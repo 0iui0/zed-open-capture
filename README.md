29febda28c3eaf0df99a8fa5b01d45da6634551d
Yelloooowww <cococing0324@gmail.com>
2022-04-12 14:35:44 +0800

    update README

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 ## Description
 This package lets you use the ZED stereo camera with ROS. Compared to the [official package](https://github.com/stereolabs/zed-ros-wrapper), this package doesn't require CUDA.
 
-It outputs the camera left and right raw images, depth image and colorful point cloud.
+It outputs the camera left and right raw images(`sensor_msgs/Image`), depth image(`sensor_msgs/Image`) and colorful point cloud(`sensor_msgs/PointCloud2`), point cloud with frame_id `zed_mini`.
 
-Tested on `Ubuntu 20.04` & `ROS noetic`.
+Tested on `Ubuntu 20.04` & `ROS noetic` with `ZED mini`.
 
-Part of code modify from [ZED Open Capture](https://github.com/stereolabs/zed-open-capture)
+Part of code modify from [ZED Open Capture](https://github.com/stereolabs/zed-open-capture).
 
 ## Install prerequisites
-`$ sudo apt install -y build-essential \
-                      cmake \
-                      libusb-1.0-0-dev libhidapi-libusb0 libhidapi-dev \
+`$ sudo apt install -y build-essential
+                      cmake
+                      libusb-1.0-0-dev libhidapi-libusb0 libhidapi-dev
                       libopencv-dev libopencv-viz-dev`
 
 
--- src/zed_ros_node.h
+++ src/zed_ros_node.h
@@ -23,55 +23,20 @@
 #include <ros/timer.h>
 #include <ros/package.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_listener.h>
 #include <geometry_msgs/Vector3.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/PointStamped.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseArray.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <std_srvs/Trigger.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
 
 // cv_bridge
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
 // Pcl load and ros
-#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
-#include <pcl/io/ply_io.h>
-#include <pcl/io/io.h>
-#include <pcl/io/obj_io.h>
-#include <pcl/io/vtk_lib_io.h>
-#include <pcl/PolygonMesh.h>
-#include <pcl/common/centroid.h>
 #include <pcl/common/common.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/filters/filter.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/filters/radius_outlier_removal.h>
-#include <pcl/filters/conditional_removal.h>
 #include <pcl/point_cloud.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/kdtree/kdtree.h>
-// Pcl icp
-#include <pcl/registration/icp.h>
-// Pcl downsampling
-#include <pcl/filters/voxel_grid.h>
 
 using namespace ros;
 using namespace pcl;
